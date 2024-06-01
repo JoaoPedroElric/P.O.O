5eb1348f3863bfab3d0277abe5870280bc821599
JoaoPedroElric <joaopedroxbr14@gmail.com>
2024-06-01 13:05:38 -0300

    Feito - Area do quadrado

---
+++ atividades/AreaDoQuadrado.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class AreaDoQuadrado{
+
+    public static void main(String[] args) {
+        
+        double lado, area;
+        
+        System.out.printf("INFORME O TAMANHO DO LADO DO QUADRADO: ");
+        Scanner imput = new Scanner(System.in);
+        
+        lado = imput.nextDouble();
+        // area = lado * lado;
+
+        // Ou.
+        area = Math.pow(lado, 2);
+
+        System.out.println("A AREA DO QUADRADO É: " + area);
+
+    }
+
+}
