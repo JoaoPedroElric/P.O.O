2db27bd6735b9cbb5307c5195958186fb9b7256e
JoaoPedroElric <joaopedroxbr14@gmail.com>
2024-06-03 13:22:40 -0300

    Feito - Inverter números

---
+++ atividades/InverterNumeros.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class InverterNumeros {
+
+    public static void main(String[] args) {
+        
+        int numeroOriginal, numeroInvertido = 0;
+
+        Scanner imput = new Scanner(System.in);
+        System.out.printf("Digite um nemero inteiro para ser invertido: ");
+        numeroOriginal = imput.nextInt();
+
+        
+        while (numeroOriginal > 0){
+
+            numeroInvertido = numeroInvertido * 10 + numeroOriginal % 10;
+            numeroOriginal /= 10; 
+        }
+
+        System.out.println("Número invertido: " + numeroInvertido);
+
+        
+
+    }
+
+}
