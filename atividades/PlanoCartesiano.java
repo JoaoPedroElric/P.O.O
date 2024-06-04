aed23770cecfeba5bf8cc5afa2c0018a60e5169b
JoaoPedroElric <joaopedroxbr14@gmail.com>
2024-06-04 08:55:17 -0300

    Feito - Plano cartesiano

---
+++ atividades/PlanoCartesiano.java
@@ -0,0 +1,40 @@
+
+import java.util.Scanner;
+
+public class PlanoCartesiano {
+
+    public static void main(String[] args) {
+    
+    int pontoX, pontoY, pontoX2, pontoY2;
+
+    Scanner imput = new Scanner(System.in);
+    System.out.printf("Informe o valor do ponto x no plano cartesiano: ");
+    pontoX = imput.nextInt();
+
+    System.out.printf("Informe o valor do ponto y no plano cartesiano: ");
+    pontoY = imput.nextInt();
+
+    System.out.printf("Informe o valor do segundo ponto x no plano cartesiano: ");
+    pontoX2 = imput.nextInt();
+
+    System.out.printf("Informe o valor do segundo ponto y no plano cartesiano: ");
+    pontoY2 = imput.nextInt();
+
+    if(pontoX > pontoX2) {
+        System.out.printf("O ponto (%d,%d) está a esquerda do ponto (%d,%d)\n", pontoX2, pontoY2, pontoX, pontoY);
+    } else if(pontoX < pontoX2) {
+        System.out.printf("O ponto (%d,%d) está a direita do ponto (%d,%d)\n", pontoX2, pontoY2, pontoX, pontoY);
+    } else {
+        System.out.printf("O ponto (%d,%d) está na mesma posição (x) do ponto (%d,%d)\n", pontoX2, pontoY2, pontoX, pontoY);
+    }
+
+    if(pontoY > pontoY2) {
+        System.out.printf("O ponto (%d,%d) está abaixo do ponto (%d,%d)\n", pontoX2, pontoY2, pontoX, pontoY);
+    } else if(pontoX < pontoX2) {
+        System.out.printf("O ponto (%d,%d) está acima do ponto (%d,%d)\n", pontoX2, pontoY2, pontoX, pontoY);
+    } else {
+        System.out.printf("O ponto (%d,%d) está na mesma altura (y) do ponto (%d,%d)\n", pontoX2, pontoY2, pontoX, pontoY);
+    }
+
+    }
+}
