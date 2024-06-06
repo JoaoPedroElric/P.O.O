125e914c8da37df219b7c973a7f9b5cfa8770134
JoaoPedroElric <joaopedroxbr14@gmail.com>
2024-06-06 14:43:00 -0300

    Feito

--- unidade1/aula02/ValidarCartao.java
+++ unidade1/aula02/ValidarCartao.java
@@ -9,30 +9,37 @@ public class ValidarCartao {
         int mes, ano, anoAtual, mesAtual;
         boolean expirado;
 
-        Calendar calendar = Calendar.getInstance();
+        Calendar calendar = Calendar.getInstance(); // A função calendar vai pegar a data atual do computador.
 
+        // ---------- Obtendo a data atual ----------
         anoAtual = calendar.get(calendar.YEAR);
-        mesAtual = calendar.get(calendar.MONTH);
+        mesAtual = 1 + calendar.get(calendar.MONTH); // Mes começa em 0 para ser usado em um vetor.
         Scanner imput = new Scanner(System.in);
 
-        do{
-            System.out.printf("Digite seu nome: ");
-            nome = imput.nextLine();
+        
+        
+        System.out.printf("Digite seu nome: ");
+        nome = imput.nextLine();
 
             
-            System.out.printf("Digite o número do cartão: ");
-            numero = imput.nextLine();
+        System.out.printf("Digite o número do cartão: ");
+        numero = imput.nextLine();
 
-            System.out.printf("Digite seu codigo: ");
-            codigo = imput.nextLine();
+        System.out.printf("Digite seu codigo: ");
+        codigo = imput.nextLine();
 
+        do {
             System.out.printf("Digite o mês: ");
             mes = imput.nextInt();
 
             System.out.printf("Digite o ano: ");
             ano = imput.nextInt();
 
+            // Checa se a data esta na validade.
             expirado = (ano < anoAtual) || (ano == anoAtual) && (mes < mesAtual);
+            if (expirado){
+                System.out.println("!!A data está expirada, ou errada!!");
+            }
 
             System.out.printf("%d/%d\n", mesAtual, anoAtual);
 
