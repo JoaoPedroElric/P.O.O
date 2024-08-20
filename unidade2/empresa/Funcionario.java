dc88c0c12ef9023eb013380f7bd3d282abebdb39
JoaoPedroElric <joaopedroxbr14@gmail.com>
2024-08-20 14:00:18 -0300

    Aula

---
+++ unidade2/empresa/Funcionario.java
@@ -0,0 +1,8 @@
+public class Funcionario extends Pessoas {
+    private double salario;
+
+    public Funcionario(double salario) {
+        this.salario = salario;
+    }
+
+}
---
+++ unidade2/empresa/Pessoas.java
@@ -0,0 +1,19 @@
+
+public class Pessoas {
+    private String nome;
+    private int identidade;
+
+    public Pessoas(String nome, int identidade) {
+    this.nome = nome;
+    this.identidade = identidade;
+    }
+
+    public String getnome() {
+        return nome;
+    }
+
+    public int getidentidade() {
+        return identidade;
+    }
+
+}
